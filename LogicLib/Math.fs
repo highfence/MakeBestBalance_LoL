1a5a5c9dee459d0db1e19c67a5681b492489bc0c
Lee Kwenwon <highfence@naver.com>
2018-08-31 21:33:47 +0900

    [ADD] Math.fs

---
+++ LogicLib/Math.fs
@@ -0,0 +1,11 @@
+﻿namespace LogicLib
+
+module FMath =
+
+    let rec insertions x = function
+        | []             -> [[x]]
+        | (y :: ys) as l -> (x::l)::(List.map (fun x -> y::x) (insertions x ys))
+
+    let rec permutations = function
+        | []      -> seq [ [] ]
+        | x :: xs -> Seq.concat (Seq.map (insertions x) (permutations xs))
